41ccb4b24983d3347c0c20df593622a36782982d
Dylan Stocker <36962371+Shloicker@users.noreply.github.com>
2019-03-19 18:04:41 +0000

    Update random_sequence_analysis.py

--- random_sequence_analysis.py
+++ random_sequence_analysis.py
@@ -2,21 +2,28 @@ import random
 import ferry
 import matplotlib.pyplot as plt
 
+# ------- Program that analyses performance of different rules with randomly generated sequences ----------
+
+# the number of lanes and lane lengths are hard coded
 numLanes = 85
 lane_length = 3000
 
+# we create a list of loading rules to iterate over. The list of strings will be used in the graph later
 loading_rules = [ferry.get_first_lane, ferry.get_emptiest_lane, ferry.get_fullest_lane, ferry.get_random_lane, ferry.get_most_suitable_lane]
 loading_rule_names = ["First", "Emptiest", "Fullest", "Random", "Most Suitable"]
 
-repetitions = 1000
+repetitions = 10000                  # how many random sequences we will create for each loading rule
 
-rule_measures = []
+# -- Data Gathering for Simple Queue System --
+
+rule_measures = []                  # a list containing the measures for each rule
 for rule in loading_rules:
-    measures = []
+    measures = []                   # a list containing measures of total car length in overflow in each repetition
     for i in range(repetitions):
-        lanes = [[] for i in range(numLanes)]
+        lanes = [[] for i in range(numLanes)]                    # resetting our ferry and overflow park at each repetition
         cars_in_overflow = []
 
+        # generating a random sequence of cars
         small_cars = [round(350 + 50*random.random()) for i in range(100)]
         medium_cars = [round(400 + 50*random.random()) for i in range(200)]
         large_cars = [round(450 + 50*random.random()) for i in range(100)]
@@ -32,6 +39,7 @@ for rule in loading_rules:
     average = sum(measures)/repetitions
     rule_measures.append(average)
 
+# plotting a bar chart of the loading rules with simple queue
 f = plt.figure()
 plt.bar(range(len(loading_rule_names)), rule_measures)
 plt.xticks(range(len(loading_rule_names)), loading_rule_names)
@@ -40,6 +48,9 @@ plt.ylabel("Total Car Length in Overflow")
 plt.title("Average Measures of Rules with Random Sequences, Simple Queuing")
 plt.savefig("Average Measures of Loading Rules with Random Sequences and Simple Queue.pdf")
 
+# -- Data Gathering for Five Queue System --
+
+# code virtually identical to previous --
 rule_measures = []
 for rule in loading_rules:
     measures = []
@@ -70,7 +81,10 @@ plt.ylabel("Total Car Length in Overflow")
 plt.title("Average Measures of Rules with Random Sequences, Five Queues")
 plt.savefig("Average Measures of Rules with Random Sequences, Five Queues.pdf")
 
-measures = [[] for i in range(50)]
+# -- Data Gathering for Iterated Process --
+
+# here we generate a large number of random sequences and perform the iterated process on each of them 50 times with 50 different k values
+measures = [[] for i in range(50)]       # a list of 50 lists, each containing measures of each k value
 for i in range(repetitions):
     small_cars = [round(350 + 50*random.random()) for i in range(100)]
     medium_cars = [round(400 + 50*random.random()) for i in range(200)]
@@ -86,10 +100,11 @@ for i in range(repetitions):
         cars_in_overflow = []
 
         ferry.iterated_process_no_input(ferry.get_fullest_lane, cars, lanes, lane_length, cars_in_overflow, k)
-        measures[k - 1].append(sum(cars_in_overflow))
+        measures[k - 1].append(sum(cars_in_overflow))     # adding the measure to the appropriate list in 'measures'
 
-averages = [sum(k_measures)/repetitions for k_measures in measures]
+averages = [sum(k_measures)/repetitions for k_measures in measures]  # generating the averages for each k value
 
+# plotting a line graph of the performance of each k value
 f = plt.figure()
 plt.plot(range(1, 51), averages)
 plt.xlabel("k Values")
